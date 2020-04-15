4b2a78d618a0dc44f4a8e8eeba8df8e66910634f
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-15 20:27:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,16 @@
 def addition(num1, num2)
-
+  sum1 = num1 + num2
+  return sum2
 end
 
 def subtraction(num1, num2)
-
+  sum2 = num1 - num2
+  return sum2
 end
 
 def division(num1, num2)
-
+  sumthree = num1 / num2
+  return sumthree
 end
 
 def multiplication(num1, num2)
